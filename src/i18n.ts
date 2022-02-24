e1c1552e426aff2212ca8eccad1b609f8fa8b9a2
ushiboy <ushiboy.dev@gmail.com>
2022-02-24 23:10:12 +0900

    separate translation file

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,14 +1,18 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import en from "./translations/en.json";
-import ja from "./translations/ja.json";
+import common_en from "./translations/en/common.json";
+import common_ja from "./translations/ja/common.json";
+import error_en from "./translations/en/error.json";
+import error_ja from "./translations/ja/error.json";
 
 const resources = {
   en: {
-    translation: en,
+    common: common_en,
+    error: error_en,
   },
   ja: {
-    translation: ja,
+    common: common_ja,
+    error: error_ja,
   },
 };
 
--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import * as S from "./style";
 
 export default function About() {
+  const { t } = useTranslation("error");
   return (
     <S.Root>
       <S.Title>About</S.Title>
+      <S.Message>{t("NG")}</S.Message>
     </S.Root>
   );
 }
